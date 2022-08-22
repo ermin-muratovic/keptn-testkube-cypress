b03bbee9a07403d973625aacc0b3859f4232fb9c
Ermin Muratovic <ermin.muratovic@gmail.com>
2022-08-22 14:19:38 +0200

    Click notification

--- cypress/integration/load-bridge.js
+++ cypress/integration/load-bridge.js
@@ -1,18 +1,14 @@
 describe("Keptn's Bridge", () => {
   it("successfully loads dashboard", () => {
     cy.visit("http://api-gateway-nginx.keptn.svc.cluster.local/bridge", {
-            auth: {
-                username: Cypress.env('BRIDGE_BASICAUTH_USER'),
-                password: Cypress.env('BRIDGE_BASICAUTH_PASSWORD')
-            }
-        });
+        auth: {
+            username: Cypress.env('BRIDGE_BASICAUTH_USER'),
+            password: Cypress.env('BRIDGE_BASICAUTH_PASSWORD')
+        }
+    });
 
-    cy.contains(
-      "Keptn"
-    ).should("be.visible");
-
-    cy.contains(
-      "Create a new project"
-    ).should("be.visible");
+    cy.contains("Keptn").should("be.visible");
+    cy.get('button').contains('Accept').click();
+    cy.contains("Create a new project").should("be.visible");
   });
 });
